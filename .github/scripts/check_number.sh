efe677a2eb882e09ce8302798beefa137078259c
Landon Harris <lharri73@vols.utk.edu>
2022-07-23 17:14:48 -0400

    update return val

--- .github/scripts/check_number.sh
+++ .github/scripts/check_number.sh
@@ -4,8 +4,8 @@ num=`find raw -maxdepth 2 -mindepth 2 -type d -printf '%f\n' | wc -l`
 actual=`ls -1q generated/*.zip`
 
 if [ "$num" == "$actual" ]; then
-    return 0;
+    exit 0;
 else
     echo "Number of generated models is not equal to number of simulink models."
-    return 1;
+    exit 1;
 fi
